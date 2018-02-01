6466d98d80bba912d9f5aa4eb5e084b6a8bfbab1
jiemii <35835853+jiemii@users.noreply.github.com>
2018-01-31 21:05:40 -0800

    Update puzzle 1.js

--- puzzle 1.js
+++ puzzle 1.js
@@ -14,24 +14,3 @@ up
 up
 right
 right
-
-
-
-// js
-
-down();
-down();
-down();
-down();
-down();
-down();
-down();
-right();
-right();
-right();
-right();
-right();
-up();
-up();
-right();
-right();
